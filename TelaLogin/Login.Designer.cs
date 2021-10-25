b52394bcbe35be35cb1f59b22eff26992d3a78c9
David Jonathan <david.jonn1@hotmail.com>
2021-10-25 08:48:54 -0300

    Adicionar arquivos de projeto.

---
+++ TelaLogin/Login.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace TelaLogin
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxSenha = new System.Windows.Forms.TextBox();
+            this.labelSenha = new System.Windows.Forms.Label();
+            this.labelEsqueci = new System.Windows.Forms.Label();
+            this.buttonLogar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelLogin.Location = new System.Drawing.Point(33, 40);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(55, 21);
+            this.labelLogin.TabIndex = 0;
+            this.labelLogin.Text = "Login:";
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(94, 42);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(265, 23);
+            this.textBoxLogin.TabIndex = 1;
+            // 
+            // textBoxSenha
+            // 
+            this.textBoxSenha.Location = new System.Drawing.Point(94, 86);
+            this.textBoxSenha.Name = "textBoxSenha";
+            this.textBoxSenha.Size = new System.Drawing.Size(265, 23);
+            this.textBoxSenha.TabIndex = 3;
+            // 
+            // labelSenha
+            // 
+            this.labelSenha.AutoSize = true;
+            this.labelSenha.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelSenha.Location = new System.Drawing.Point(33, 84);
+            this.labelSenha.Name = "labelSenha";
+            this.labelSenha.Size = new System.Drawing.Size(58, 21);
+            this.labelSenha.TabIndex = 2;
+            this.labelSenha.Text = "Senha:";
+            // 
+            // labelEsqueci
+            // 
+            this.labelEsqueci.AutoSize = true;
+            this.labelEsqueci.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelEsqueci.Location = new System.Drawing.Point(94, 121);
+            this.labelEsqueci.Name = "labelEsqueci";
+            this.labelEsqueci.Size = new System.Drawing.Size(115, 13);
+            this.labelEsqueci.TabIndex = 4;
+            this.labelEsqueci.Text = "Esqueci minha senha";
+            // 
+            // buttonLogar
+            // 
+            this.buttonLogar.Location = new System.Drawing.Point(266, 152);
+            this.buttonLogar.Name = "buttonLogar";
+            this.buttonLogar.Size = new System.Drawing.Size(93, 33);
+            this.buttonLogar.TabIndex = 5;
+            this.buttonLogar.Text = "Logar";
+            this.buttonLogar.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(387, 202);
+            this.Controls.Add(this.buttonLogar);
+            this.Controls.Add(this.labelEsqueci);
+            this.Controls.Add(this.textBoxSenha);
+            this.Controls.Add(this.labelSenha);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.labelLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxSenha;
+        private System.Windows.Forms.Label labelSenha;
+        private System.Windows.Forms.Label labelEsqueci;
+        private System.Windows.Forms.Button buttonLogar;
+    }
+}
+
